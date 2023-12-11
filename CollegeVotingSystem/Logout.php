c7c2a76f300a0cefd82bc3e5c3518a9a233d5cdc
bishallopchan12 <137765665+bishallopchan12@users.noreply.github.com>
2023-12-11 16:42:41 +0545

    Delete CollegeVotingSystem directory

--- CollegeVotingSystem/Logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$conn = mysqli_connect('localhost', 'root', '', 'voting_system');
-
-  echo "<script> window.open('Login.php','_self') </script>";
-
-session_destroy();
-?>
--- CollegeVotingSystem/adminLogout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$conn = mysqli_connect('localhost', 'root', '', 'voting_system');
-
-  echo "<script> window.open('admin.php','_self') </script>";
-
-session_destroy();
-?>
